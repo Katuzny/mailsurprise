9d803a7941f2e1f4325252f4be13e1a093fe8443
katuzny <katuznychristophe@gmail.fr>
2019-05-15 13:55:46 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require 'config.php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<title>SURPRISE</title>
+</head>
+<body>
+
+</body>
+</html>>
