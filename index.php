ca23b8795d39e862bbdbb61b858d0f56e4ab1262
katuzny <katuznychristophe@gmail.fr>
2019-05-15 15:46:41 +0200

    fin

--- index.php
+++ index.php
@@ -1,16 +1,39 @@
 <?php
 require 'config.php';
+require 'vendor/autoload.php'; 
+session_start();
+	
 
-?>
+if (isset($_SESSION["mail"])&&($_SESSION["mail"])=="ok") 
+	{
+		envoiMail('Surprise','contact@apprendre.com',['html'=>'Si tu reçois ce message je passe au 2']);	
+	}
+	else
+	{
+		echo "Rafraichir la page pour votre surprise";
+	}
 
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width">
-	<title>SURPRISE</title>
-</head>
-<body>
 
-</body>
-</html>>
+
+
+	function envoiMail($objet, $mailto, $msg, $cci = true)
+	{
+		if(!is_array($mailto)){
+			$mailto = [ $mailto ];
+		}
+	
+	
+	$transport = (new Swift_SmtpTransport('smtp.gmail.com', 587, 'tls'))
+	->setUsername($mailusername)
+	->setPassword($mailpassword);
+	
+	$mailer = new Swift_Mailer($transport);
+
+	$message = (new Swift_Message($objet))
+		->setFrom([$defaultmail]);
+	if ($cci){
+		$message->setBcc($mailto);
+	}else{
+		$message->setto($mailto);
+	}
+}
